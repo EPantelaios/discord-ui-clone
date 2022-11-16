ecf58898139255c915cb547389f31b327666b9e7
evripant <evripidispantelaios@gmail.com>
2022-11-17 00:02:46 +0200

    add navbar text for header and hamburger menu

---
+++ src/components/utils/NavBarText.ts
@@ -0,0 +1,23 @@
+const navBarHamburgerMenuContent = [
+  'Home',
+  'Download',
+  'Nitro',
+  'Discover',
+  'Safety',
+  'Mod Academy',
+  'Support',
+  'Blog',
+  'Careers',
+];
+
+const navBarContent = [
+  'Download',
+  'Nitro',
+  'Discover',
+  'Safety',
+  'Support',
+  'Blog',
+  'Careers',
+];
+
+export { navBarHamburgerMenuContent, navBarContent };
