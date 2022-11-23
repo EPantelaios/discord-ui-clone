f155ddcf6330fa0e2261c1b3bb732b921c3d45d2
evripant <evripidispantelaios@gmail.com>
2022-11-23 23:08:34 +0200

    add all required fonts

---
+++ src/config/fonts.ts
@@ -0,0 +1,10 @@
+const fonts = {
+  primary: `"Whitney","Helvetica Neue","Helvetica","Arial",sans-serif`,
+  display: `"Ginto Normal","Whitney","Helvetica Neue","Helvetica","Arial",sans-serif`,
+  headline: `"Ginto Nord","Whitney","Helvetica Neue","Helvetica","Arial",sans-serif`,
+  monospace: `"Menlo","Consolas","Monaco",monospace`,
+  glitch: `"ArchivoBlack","Whitney","Helvetica Neue","Helvetica","Arial",sans-serif`,
+  cursive: `"Reenie Beanie","Comic Sans MS",cursive,sans-serif`,
+};
+
+export default fonts;
