a2e4a50685b05f9c3951cfbe17096a0d2ea38013
evripant <evripidispantelaios@gmail.com>
2022-11-25 20:00:12 +0200

    feat: add footer text content to an array

---
+++ src/utils/footerText.ts
@@ -0,0 +1,30 @@
+const FooterText = [
+  { title: 'Product', items: ['Download', 'Nitro', 'Status'] },
+  { title: 'Company', items: ['About', 'Jobs', 'Branding', 'Newsroom'] },
+  {
+    title: 'Resources',
+    items: [
+      'College',
+      'Support',
+      'Safety',
+      'Blog',
+      'Feedback',
+      'Developers',
+      'Streamkit',
+    ],
+  },
+  {
+    title: 'Policies',
+    items: [
+      'Terms',
+      'Privacy',
+      'Cookie Settings',
+      'Guidelines',
+      'Acknowledgements',
+      'Licenses',
+      'Moderation',
+    ],
+  },
+];
+
+export default FooterText;
