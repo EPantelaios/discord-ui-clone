0cc58bf6f03dbac90e9fc473c44c1da820f1cf83
evripant <evripidispantelaios@gmail.com>
2022-11-29 01:47:51 +0200

    feat: add router navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Footer from './components/Homepage/Footer/Footer';
 import Header from './components/Homepage/Layout/Header/Header';
 import MainContent from './components/Homepage/MainContent/MainContent';
@@ -5,9 +7,21 @@ import MainContent from './components/Homepage/MainContent/MainContent';
 function App() {
   return (
     <>
-      <Header />
-      <MainContent />
-      <Footer />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <MainContent />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        {/* <Route path="/register" element={<RegisterPage />} /> */}
+      </Routes>
     </>
   );
 }
