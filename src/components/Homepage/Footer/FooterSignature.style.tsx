9757fc51f74ce19a49ecf57e8f4a9b6209e52a98
evripant <evripidispantelaios@gmail.com>
2022-11-29 01:50:05 +0200

    feat: add footer last section

--- src/components/Homepage/Footer/FooterSignature.style.tsx
+++ src/components/Homepage/Footer/FooterSignature.style.tsx
@@ -2,6 +2,15 @@ import styled from 'styled-components';
 
 import colors from '../../../config/colors';
 
+export const HeaderSpacer = styled.div`
+  width: 100%;
+  height: 1px;
+  margin-top: 1.5rem;
+  margin-bottom: 1rem;
+  background: ${colors.blurple};
+  margin-top: 2.5rem;
+`;
+
 export const FooterSignatureContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -10,10 +19,19 @@ export const FooterSignatureContainer = styled.div`
   margin-top: 2rem;
 `;
 
-export const HeaderSpacer = styled.div`
-  width: 100%;
-  height: 1px;
-  margin-top: 1.5rem;
-  margin-bottom: 1rem;
-  background: ${colors.blurple};
+export const SignUpButton = styled.span`
+  all: inherit;
+  padding: 0.6rem 0.8rem;
+  font-size: 14px;
+  color: ${colors.white};
+  background-color: ${colors.blurple};
+  border: 1px solid ${colors.blurple};
+  outline: 1px solid ${colors.blurple};
+
+  :hover,
+  :active {
+    background-color: ${colors.lightpurple};
+    border: 1px solid ${colors.lightpurple};
+    outline: 1px solid ${colors.lightpurple};
+  }
 `;
--- src/components/Homepage/Footer/FooterSignature.tsx
+++ src/components/Homepage/Footer/FooterSignature.tsx
@@ -1,13 +1,25 @@
+import HomePageButton from '../../UI/HomePageButton';
+import { NavBarLogoIcon } from '../Layout/NavBarContent/NavBarLogo.style';
+import { Link } from 'react-router-dom';
 import {
   FooterSignatureContainer,
   HeaderSpacer,
+  SignUpButton,
 } from './FooterSignature.style';
 
 function FooterSignature() {
   return (
-    <FooterSignatureContainer>
+    <>
       <HeaderSpacer />
-    </FooterSignatureContainer>
+      <FooterSignatureContainer>
+        <NavBarLogoIcon />
+        <Link to="register">
+          <HomePageButton>
+            <SignUpButton>Sign Up</SignUpButton>
+          </HomePageButton>
+        </Link>
+      </FooterSignatureContainer>
+    </>
   );
 }
 
