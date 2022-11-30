bbf41721404c2758ae39ea39cbdd163952fcb231
evripant <evripidispantelaios@gmail.com>
2022-11-30 23:20:21 +0200

    add language settings

---
+++ src/utils/languages.tsx
@@ -0,0 +1,19 @@
+export const defaultLanguage = { name: 'English, USA' };
+
+const Languages = [
+  { name: 'български' },
+  { name: 'Čeština' },
+  { name: 'Dansk' },
+  { name: 'Deutsch' },
+  { name: 'Ελληνικά' },
+  { name: 'English, USA' },
+  { name: 'Español' },
+  { name: 'Suomi' },
+  { name: 'Français' },
+  { name: 'हिंदी' },
+  { name: 'Hrvatski' },
+  { name: 'Magyar' },
+  { name: 'Italiano' },
+];
+
+export default Languages;
