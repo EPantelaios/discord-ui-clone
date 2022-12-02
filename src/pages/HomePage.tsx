b39747fa48444b4176196da2473235725790e04d
evripant <evripidispantelaios@gmail.com>
2022-12-02 23:18:32 +0200

    feat: add main page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import Footer from '../components/Homepage/Footer/Footer';
+import Header from '../components/Homepage/Layout/Header/Header';
+import MainContent from '../components/Homepage/MainContent/MainContent';
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
