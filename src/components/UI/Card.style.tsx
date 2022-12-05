04ec2e2dc2052662b081e7aae16794d9b7e98a30
evripant <evripidispantelaios@gmail.com>
2022-12-05 20:03:06 +0200

    feat: add bounce-out animation for card switching

--- src/components/UI/Card.style.tsx
+++ src/components/UI/Card.style.tsx
@@ -8,4 +8,16 @@ export const CardContainer = styled.div`
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
   background-color: ${colors.darkestgrey};
   z-index: 1;
+  animation: bounceOut 0.3s;
+
+  @keyframes bounceOut {
+    0% {
+      transform: scale(0.5);
+      opacity: 0.1;
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
 `;
