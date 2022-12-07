b0997fcd84d03fab394561a39935bc95f13c74ee
evripant <evripidispantelaios@gmail.com>
2022-12-08 00:46:58 +0200

    add event listener for resize

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,15 @@ import AuthPage from './components/AuthPage/AuthPage';
 import HomePage from './pages/HomePage';
 
 function App() {
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  const vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  window.addEventListener('resize', () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-
         <Route path="/login" element={<AuthPage login />} />
         <Route path="/register" element={<AuthPage register />} />
       </Routes>
